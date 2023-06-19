5683d42598890d5d05f072ae8114a53144b60550
Afroz-Shaikh <afrozshaikh725@gmail.com>
2023-06-19 12:23:15 +0530

    Fix: Refresh Contacts

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -40,6 +40,7 @@ class _AppState extends State<App> with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'CoNet',
       theme: ThemeData(
         primaryColor: AppColor.whiteColor,

31089ab711e6db54a8dee85c30f5e8d2b6818c41
Sahil Garg <sahilg@geekyants.com>
2023-06-30 16:44:39 +0530

    Added sf pro display fonts to add in the settings tiles

--- lib/utils/custom_fonts.dart
+++ lib/utils/custom_fonts.dart
@@ -1,3 +1,4 @@
 //custom font-family
 const String kSfproRoundedFontFamily = 'SfproRounded'; //main fonts
+const String kSfproDisplayFontFamily = 'SfproDisplay'; //Secondary fonts
 const String kDreadnoughtusFontFamily = 'Dreadnoughtus'; //other font

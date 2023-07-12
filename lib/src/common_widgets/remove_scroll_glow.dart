b12810c01a6ef5400d92d4685cafae7a1a1b9c84
Sahil Garg <sahilg@geekyants.com>
2023-07-12 19:43:12 +0530

    Removed the over scroll glow appearance in the profile & editProfile pages

---
+++ lib/src/common_widgets/remove_scroll_glow.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RemoveScrollGlow extends ScrollBehavior {
+  @override
+  Widget buildOverscrollIndicator(BuildContext context, Widget child, ScrollableDetails details) {
+    return child;
+  }
+}
